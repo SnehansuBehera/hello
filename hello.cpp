80112605ad1c91c0a69b62bc7ed8f1ea7fa8e06f
SnehansuBehera <snehansbehera080903@gmail.com>
2025-08-28 10:52:43 +0530

    deleted-few-bugs

--- hello.cpp
+++ hello.cpp
@@ -4,12 +4,6 @@ using namespace std;
 int main()
 {
     cout << "Hello, World!" << endl;
-    cout << "hello babe" << endl;
-    cout << "hello babe" << endl;
-    cout << "hello babe" << endl;
-    cout << "hello babe" << endl;
-    cout << "hello babe" << endl;
-    cout << "hello babe" << endl;
 
     return 0;
 }
